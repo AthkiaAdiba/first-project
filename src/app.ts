5e0924abf4b81cc22477dfe2055f1e79bb1bb921
thkiaAdiba <athkiaadiba@gmail.com>
2024-11-18 15:23:08 +0600

    added routing

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,19 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
+import { StringRoutes } from './app/modules/student/student.route';
 const app: Application = express();
 
 // parsers
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req: Request, res: Response) => {
-  const a = 10;
+// application routes
+app.use('/api/v1/students', StringRoutes);
 
+const getAController = (req: Request, res: Response) => {
   res.send('Hello World!');
-});
+};
+
+app.get('/', getAController);
 
 export default app;
