1772d999293ec4f148be1b6bd8d04b383af2e579
thkiaAdiba <athkiaadiba@gmail.com>
2024-12-01 10:58:41 +0600

    added academic controllers and service

---
+++ src/app/modules/academicSemester/academicSemester.route.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import { AcademicSemesterControllers } from './academicSemester.controller';
+import { AcademicSemesterValidations } from './academicSemester.validation';
+import validateRequest from '../../middlewares/validateRequest';
+
+const router = express.Router();
+
+router.post(
+  '/create-academic-semester',
+  validateRequest(
+    AcademicSemesterValidations.createAcademicSemesterValidationSchema,
+  ),
+  AcademicSemesterControllers.createAcademicSemester,
+);
+
+export const AcademicSemesterRoutes = router;
