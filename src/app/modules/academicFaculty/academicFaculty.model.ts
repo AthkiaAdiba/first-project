11adefe98fd9bd5927d033e5588ab8f6074dcbc7
thkiaAdiba <athkiaadiba@gmail.com>
2024-12-15 21:00:40 +0600

    added academic faculty services

--- src/app/modules/academicFaculty/academicFaculty.model.ts
+++ src/app/modules/academicFaculty/academicFaculty.model.ts
@@ -1,16 +1,20 @@
-import { Schema } from "mongoose";
-import { TAcademicFaculty } from "./academicFaculty.interface";
+import { model, Schema } from 'mongoose';
+import { TAcademicFaculty } from './academicFaculty.interface';
 
-const academicFacultySchema = new Schema<TAcademicFaculty>({
+const academicFacultySchema = new Schema<TAcademicFaculty>(
+  {
     name: {
-        type: String,
-        required: true,
-        unique: true,
-    }
-},
-{
+      type: String,
+      required: true,
+      unique: true,
+    },
+  },
+  {
     timestamps: true,
-}
-)
+  },
+);
 
-export const AcademicFaculty = 
+export const AcademicFaculty = model<TAcademicFaculty>(
+  'AcademicFaculty',
+  academicFacultySchema,
+);
