dbbc01a110f9938330fa2dad35027a585691457e
thkiaAdiba <athkiaadiba@gmail.com>
2024-12-26 21:29:18 +0600

    added more secure logics

--- src/app/middlewares/auth.ts
+++ src/app/middlewares/auth.ts
@@ -5,6 +5,7 @@ import httpStatus from 'http-status';
 import jwt, { JwtPayload } from 'jsonwebtoken';
 import config from '../config';
 import { TUserRole } from '../modules/user/user.interface';
+import { User } from '../modules/user/user.model';
 
 const auth = (...requiredRoles: TUserRole[]) => {
   return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
@@ -21,7 +22,38 @@ const auth = (...requiredRoles: TUserRole[]) => {
       config.jwt_access_secret as string,
     ) as JwtPayload;
 
-    const role = decoded.role;
+    const { role, userId, iat } = decoded;
+
+    //   checking if the user is exists
+    const user = await User.isUserExistsByCustomId(userId);
+
+    if (!user) {
+      throw new AppError(httpStatus.NOT_FOUND, 'This user is not found!');
+    }
+
+    //   checking if user is already delete
+    const iDeleted = user?.isDeleted;
+
+    if (iDeleted) {
+      throw new AppError(httpStatus.FORBIDDEN, 'This user is deleted!');
+    }
+
+    // //   checking if user is already blocked
+    const userStatus = user?.status;
+
+    if (userStatus === 'blocked') {
+      throw new AppError(httpStatus.FORBIDDEN, 'This user is blocked!');
+    }
+
+    if (
+      user.passwordChangeAt &&
+      User.isJWTIssuedBeforePasswordChanged(
+        user.passwordChangeAt,
+        iat as number,
+      )
+    ) {
+      throw new AppError(httpStatus.UNAUTHORIZED, 'You are not authorized!');
+    }
 
     if (requiredRoles && !requiredRoles.includes(role)) {
       throw new AppError(httpStatus.UNAUTHORIZED, 'You are not authorized!');
